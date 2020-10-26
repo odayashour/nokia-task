6cb659ebee85ba884c88e4ad073b62f532c1756b
Oday Ashour <OdayA@exalt.ps>
2020-10-26 15:57:10 +0200

    Third Commit

--- src/main/java/com/nokia/task/service/PersonService.java
+++ src/main/java/com/nokia/task/service/PersonService.java
@@ -2,6 +2,8 @@ package com.nokia.task.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 import com.nokia.task.model.Person;
 import org.springframework.stereotype.Service;
@@ -11,45 +13,62 @@ public class PersonService {
 
     private static List<Person> persons;
 
-    public PersonService(){
+    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private final Lock readLock = rwl.readLock();
+    private final Lock writeLock = rwl.writeLock();
+
+    public PersonService() {
         persons = new ArrayList<Person>();
     }
-    public List<Person> getAll(){
-        return persons;
+
+    public List<Person> getAll() {
+
+        try {
+            readLock.lock();
+            return persons;
+        } finally {
+            readLock.unlock();
+        }
     }
 
-    public List<Person> findByName(String name){
-        return persons.stream().filter(p-> p.getName().equals(name))
-                               .collect(Collectors.toList());
+    public List<Person> findByName(String name) {
+
+        try {
+            readLock.lock();
+            return persons.stream().filter(p -> p.getName().equals(name)).collect(Collectors.toList());
+        } finally {
+            readLock.unlock();
+        }
     }
 
-    public synchronized int deleteByName(String name){
+    public int deleteByName(String name) {
 
         List<Person> listPersons = findByName(name);
+        try {
+            writeLock.lock();
 
-        if (listPersons.size()>0){
-            persons.removeAll(listPersons);
+            if (listPersons.size() > 0) {
+                persons.removeAll(listPersons);
+            }
+            return listPersons.size();
+        } finally {
+            writeLock.unlock();
         }
-        return listPersons.size();       
     }
 
-    /* TaskExceptionHandler class handle the thrown OutOfMemoryError */
-    public synchronized boolean addPerson(String id,String name) throws OutOfMemoryError {
-    
-        boolean isExists= persons.stream()
-                                 .filter(p-> p.getId().equals(id))
-                                 .findFirst().isPresent();                            
-        if (isExists == false){                  
-            int sizeOfId =  8 * (int) ((((id.length()) * 2) + 45) / 8);
-            int sizeOfName =  8 * (int) ((((name.length()) * 2) + 45) / 8);
-            int sizeofObject= sizeOfId + sizeOfName + 16 ;
+    public boolean addPerson(String id, String name) throws OutOfMemoryError {
 
-            if(Runtime.getRuntime().freeMemory() > sizeofObject){
+        try {
+            writeLock.lock();
+            boolean isExists = persons.stream().filter(p -> p.getId().equals(id)).findFirst().isPresent();
+            if (isExists == false){                  
                 Person person = new Person(id,name);     
                 persons.add(person);
-                return true;
-            }     
+                return true;            
+            }
+            return false;   
+        } finally{
+            writeLock.unlock();
         }
-        return false;      
     }    
 }
--- src/test/java/com/nokia/task/PersonServiceTest.java
+++ src/test/java/com/nokia/task/PersonServiceTest.java
@@ -4,8 +4,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
+import com.nokia.task.model.Person;
 import com.nokia.task.service.PersonService;
 
 import org.junit.Test;
@@ -13,64 +23,116 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PersonServiceTest {
-      
+
     private PersonService personService = new PersonService();
     private Random random = new Random();
 
     @Test
-    public void addTest()throws Exception{
+    public void addTest() throws Exception {
 
         String randomId = "P" + random.nextInt();
-        assertTrue(personService.addPerson(randomId,"Person"));   
+        assertTrue(personService.addPerson(randomId, "Person"));
     }
 
     @Test
-    public void addWithExistsIdTest()throws Exception{
+    public void addWithExistsIdTest() throws Exception {
 
         String randomId = "P" + random.nextInt();
 
-        personService.addPerson(randomId,"Person1");
-        assertFalse(personService.addPerson(randomId,"Person2"));  
+        personService.addPerson(randomId, "Person1");
+        assertFalse(personService.addPerson(randomId, "Person2"));
     }
 
     @Test
-    public void getAllTest()throws Exception{
-       
-        personService.addPerson("P" + random.nextInt(),"Person1");
-        personService.addPerson("P" + random.nextInt(),"Person2");
-        personService.addPerson("P" + random.nextInt(),"Person3");
+    public void getAllTest() throws Exception {
+
+        personService.addPerson("P" + random.nextInt(), "Person1");
+        personService.addPerson("P" + random.nextInt(), "Person2");
+        personService.addPerson("P" + random.nextInt(), "Person3");
         assertEquals(3, personService.getAll().size());
     }
 
     @Test
-    public void searchTest()throws Exception{
-       
-        personService.addPerson("P" + random.nextInt(),"Person1");
-        personService.addPerson("P" + random.nextInt(),"Person1");
-        personService.addPerson("P" + random.nextInt(),"Person2");
+    public void searchTest() throws Exception {
+
+        personService.addPerson("P" + random.nextInt(), "Person1");
+        personService.addPerson("P" + random.nextInt(), "Person1");
+        personService.addPerson("P" + random.nextInt(), "Person2");
         assertEquals(2, personService.findByName("Person1").size());
     }
 
     @Test
-    public void searchEmptyTest()throws Exception{
-       
-        personService.addPerson("P" + random.nextInt(),"Person1");
+    public void searchEmptyTest() throws Exception {
+
+        personService.addPerson("P" + random.nextInt(), "Person1");
         assertEquals(0, personService.findByName("Person2").size());
     }
 
     @Test
-    public void deleteTest()throws Exception{
+    public void deleteTest() throws Exception {
 
-        personService.addPerson("P" + random.nextInt(),"Person1");
-        personService.addPerson("P" + random.nextInt(),"Person1");
-        personService.addPerson("P" + random.nextInt(),"Person2");
+        personService.addPerson("P" + random.nextInt(), "Person1");
+        personService.addPerson("P" + random.nextInt(), "Person1");
+        personService.addPerson("P" + random.nextInt(), "Person2");
         assertEquals(2, personService.deleteByName("Person1"));
     }
 
     @Test
-    public void emptyDeleteTest()throws Exception{
+    public void emptyDeleteTest() throws Exception {
+        assertEquals(0, personService.deleteByName("Person"));
+    }
 
-        PersonService pService = new PersonService(); 
-        assertEquals(0, pService.deleteByName("Person"));
+    @Test
+    public void concurencyAddTest() throws Exception {
+        Thread thread1 = new Thread(() -> {
+        
+
+            for (int i = 0; i < 100; i++) {
+                personService.addPerson("p" + i, "name" + i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                personService.addPerson("p" + i, "name" + i);
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        assertEquals(100, personService.getAll().size());
+    }
+
+    @Test
+    public void concurencyTest() throws Exception {
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.execute(()->{
+            for (int i = 0; i < 10; i++) {
+                personService.addPerson("p" + i, "name" + i);
+            }
+        });
+
+        executorService.execute(()->{
+            for (int i = 0; i < 7; i++) {
+                personService.deleteByName("name" + i);
+            }
+        });
+        executorService.awaitTermination(1,TimeUnit.SECONDS);
+        Callable<List<Person>> taskRead = () -> {
+                List<Person> persons = new ArrayList<Person>();
+                for (int i = 7; i < 10; i++) {
+                    persons.addAll(personService.findByName("name" + i));
+                }
+                 return persons;
+        };
+
+        Future<List<Person>> future = executorService.submit(taskRead);
+        List<Person> persons = future.get();
+        assertEquals(3, persons.size());
     }
 }
